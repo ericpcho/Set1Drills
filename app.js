e89626e69175589f854bedf76044360323895a91
ericpcho <ericpcho@gmail.com>
2017-08-21 12:53:22 -0700

    Added calcYob function

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 'use strict';
 
 function whoAmI(name, age){
-  let yearOfBirth = 2017 - age;
-
-  try {
-    if(age <= 0) throw new Error("Age cannot be negative");
-
+  let yearOfBirth = calcYob(age);
     console.log(`Hi, my name is ${name} and I'm ${age} years old`);
     console.log(`I was born in ${yearOfBirth}`);
-       }
-
-  catch(error) {
-    console.log(error);
-  }
 
 }
 
+function calcYob(age){
+  return 2017 - age;
+}
+
 whoAmI("Eric", -29);
-
-
 
