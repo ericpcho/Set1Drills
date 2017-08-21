41968013fa2b549fbc32e0d65ef37250d607ef1d
Brian Besaw <bjbesaw@outlook.com>
2017-08-21 15:20:47 -0400

    added number 4

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 'use strict';
 
 function whoAmI(name, age){
-  console.log("Hi, my name is Eric and I'm 29 years old");
+  let yearOfBirth = 2017 - age;
+  console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+  console.log(`I was born in ${yearOfBirth}`);
 }
 
-whoAmI();
+whoAmI("Eric", 29);
 
 
 
