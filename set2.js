dbce7ea9cd7f56d3b3d9477fdd9316359a1dc39e
Brian Besaw <bjbesaw@outlook.com>
2017-08-21 17:00:07 -0400

    number 2

--- set2.js
+++ set2.js
@@ -1,5 +1,20 @@
+'use strict'
+
 function jediName(firstName, lastName){
   return lastName.slice(0,3) + firstName.slice(0,2);
 }
 
-console.log(jediName("Eric", "Pcholinski"));
+function beyond(num){
+  if(Math.abs(num) == Infinity){
+    return "And beyond";
+  } else if(num>0){
+    return "To infinity";
+  } else if(num<0){
+    return "To negative infinity";
+  } else{
+    return "Staying home";
+  }
+}
+
+
+console.log(beyond(-Infinity));
