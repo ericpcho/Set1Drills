38995da51192ff966f4c874e9e54ca1b9af5e42a
ericpcho <ericpcho@gmail.com>
2017-08-21 12:43:48 -0700

    Added trycatch error

--- app.js
+++ app.js
@@ -2,11 +2,21 @@
 
 function whoAmI(name, age){
   let yearOfBirth = 2017 - age;
-  console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-  console.log(`I was born in ${yearOfBirth}`);
+
+  try {
+    if(age <= 0) throw new Error("Age cannot be negative");
+
+    console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+    console.log(`I was born in ${yearOfBirth}`);
+       }
+
+  catch(error) {
+    console.log(error);
+  }
+
 }
 
-whoAmI("Eric", 29);
+whoAmI("Eric", -29);
 
 
 
