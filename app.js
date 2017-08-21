12a1651c26140438d4e25e7934f827044a2c3d58
ericpcho <ericpcho@gmail.com>
2017-08-21 13:02:34 -0700

    changed try catch statement

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 'use strict';
 
 function whoAmI(name, age){
+  try {
   let yearOfBirth = calcYob(age);
     console.log(`Hi, my name is ${name} and I'm ${age} years old`);
     console.log(`I was born in ${yearOfBirth}`);
-
+  }
+  catch(error) {
+    console.log(error);
+  }
 }
 
 function calcYob(age){
+  if(age <= 0) throw new Error("Age cannot be negative");
   return 2017 - age;
 }
 
-whoAmI("Eric", -29);
-
+whoAmI("Eric", 29);
